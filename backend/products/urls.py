ba60770cdfaf00e158b00654cd59ba1e75cdd2c9
Snex-Thadeus <lwande94@gmail.com>
2022-09-23 15:51:25 +0300

    Mixins and Generic API View

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from . import views
+from . import views 
 
 
 urlpatterns = [
-    path('', views.product_list_create_view),
+    # path('', views.product_list_create_view),
+    path('', views.product_mixin_view),
     path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()), 
     path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view()),
-    path('<int:pk>/', views.ProductDetailAPIView.as_view()),
+    # path('<int:pk>/', views.ProductDetailAPIView.as_view()),
+    path('<int:pk>/', views.product_mixin_view),
     # path('product/', views.api_post)
     # path('', views.product_alt_view), #Function Based View URL
     # path('<int:pk>/', views.product_alt_view), 
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,5 +1,4 @@
-from turtle import title
-from rest_framework import generics
+from rest_framework import generics, mixins
 from .models import Product
 from . serializers import ProductSerializer
 
@@ -65,7 +64,43 @@ class ProductDeleteAPIView(generics.DestroyAPIView):
 
 
 
-#FUNCTION BASED VIEW:
+#CLASS BASED VIEWS: No conditions
+class ProductMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        # print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        
+        if content is None:
+            content = "This is a single view doing cool stuffs"
+        serializer.save(content=content)
+
+
+product_mixin_view = ProductMixinView.as_view()
+
+
+
+
+
+#FUNCTION BASED VIEW: we write conditions
 
 @api_view(["GET", "POST"])
 def product_alt_view(request, pk=None, *args, **kwargs):
