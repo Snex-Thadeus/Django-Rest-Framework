87c8cc36efb571517622ca046120fe84060277f0
Snex-Thadeus <lwande94@gmail.com>
2022-09-24 00:11:19 +0300

    URLs,Reverse and Serializers

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.product_list_create_view),
+    path('', views.product_list_create_view, name='product-list'),
     # path('', views.product_mixin_view),
-    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()), 
+    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view(), name='product-edit'), 
     path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view()),
-    path('<int:pk>/', views.ProductDetailAPIView.as_view()),
+    path('<int:pk>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
     # path('<int:pk>/', views.product_mixin_view),
     # path('product/', views.api_post)
     # path('', views.product_alt_view), #Function Based View URL
