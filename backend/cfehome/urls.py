6adb89701da392448ea647a526dc2853738aaecd
Snex-Thadeus <lwande94@gmail.com>
2022-09-23 23:27:08 +0300

    ViewSets and Routers

--- backend/cfehome/urls.py
+++ backend/cfehome/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/product/', include('products.urls')),
+    path('api/v2/', include('cfehome.routers')),
 ]
