506257cdb0aa3d2c60d32128496ecd202082f437
Snex-Thadeus <lwande94@gmail.com>
2022-09-23 16:09:19 +0300

    Session Authentication & Permissions

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    # path('', views.product_list_create_view),
-    path('', views.product_mixin_view),
+    path('', views.product_list_create_view),
+    # path('', views.product_mixin_view),
     path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()), 
     path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view()),
-    # path('<int:pk>/', views.ProductDetailAPIView.as_view()),
-    path('<int:pk>/', views.product_mixin_view),
+    path('<int:pk>/', views.ProductDetailAPIView.as_view()),
+    # path('<int:pk>/', views.product_mixin_view),
     # path('product/', views.api_post)
     # path('', views.product_alt_view), #Function Based View URL
     # path('<int:pk>/', views.product_alt_view), 
