1e06efb1c8bcc93d0c4d5102cdf2c664314e916f
Snex-Thadeus <lwande94@gmail.com>
2022-09-28 00:41:19 +0300

    Search REST API via JS Client

--- js_client/client.js
+++ js_client/client.js
@@ -1,11 +1,18 @@
 const contentContainer = document.getElementById('content-container')
 const loginForm = document.getElementById('login-form')
+const searchForm = document.getElementById('search-form')
 const baseEndpoint = "http://localhost:8000/api"
 
 if (loginForm) {
     // handle this form
     loginForm.addEventListener('submit', handleLogin)
 }
+
+if (searchForm) {
+    // handle this form
+    searchForm.addEventListener('submit', handleSearch)
+}
+
 
 function handleLogin(event) {
     // console.log(event)
@@ -31,6 +38,37 @@ function handleLogin(event) {
         handleAuthData(authData, getProductList)
     })
 }
+
+function handleSearch(event) {
+    event.preventDefault()
+
+    let formData = new FormData(searchForm)
+    let data = Object.fromEntries(formData)
+    let searchParams = new URLSearchParams(data)
+
+    const endpoint = `${baseEndpoint}/search/?${searchParams}`
+    const headers = {
+        "Content-Type": "application/json",
+    }
+    const authToken = localStorage.getItem('access')
+    if (authToken){
+        headers['Authorization'] = `Bearer ${authToken}`
+    }
+
+    const options = {
+        method: "GET",
+        headers: headers
+    }
+    fetch(endpoint, options) 
+    .then(response=>{
+        return response.json()
+    })
+    .then(data => {
+        writeToContainer(data)
+    })
+}
+
+
 
 function handleAuthData(authData, callback) {
     localStorage.setItem('access', authData.access)
